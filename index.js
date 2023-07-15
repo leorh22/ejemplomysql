b4519a5696ffd44f61faf813a5a72e00425f6060
unknown <leonardorh.ti22@utsjr.edu.mx>
2023-07-14 22:06:06 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+var usuariosRutas= require("./rutas/usuarios");
+var session = require("express-session");
+require("dotenv").config();
+
+var app = express();
+
+app.set("view engine", "ejs");
+app.set("/web", express.static(path.join(__dirname, "web")));
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret:process.env.SECRETO_SESSION,
+    resave: true, 
+    saveUninitialized: true
+}));
+
+app.use("/", usuariosRutas);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log("Servidor en http://localhost:" + port);
+});
